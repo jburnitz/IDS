1c2a7f16b5c66d855ad6eb44f5476e734766550c
amanet2 <tmanetti1992@gmail.com>
2014-12-01 21:57:39 -0600

    I edited the pcapreader to detect UDP, as well as print more stuff

--- src/pcapreader.java
+++ src/pcapreader.java
@@ -7,19 +7,36 @@ import net.sourceforge.jpcap.net.Packet;
 import net.sourceforge.jpcap.net.IPPacket;
 import net.sourceforge.jpcap.net.TCPPacket;
 
+//edit 12_1
+import net.sourceforge.jpcap.net.UDPPacket;
+
 class PacketCaptureListener extends PacketCapture implements PacketListener{
 
 	@Override
 	public void packetArrived(Packet packet) {
 		// TODO Auto-generated method stub
 		IPPacket ipPacket = (IPPacket)packet;
+
 		if(ipPacket.getProtocol() == 6 ) //means its TCP
-			System.out.println(ipPacket.getSourceAddress() + " : " + ((TCPPacket)ipPacket).getSourcePort());
-		else
-			System.out.println("Packet is not TCP");		
+			System.out.println("TCP packet from " + ipPacket.getSourceAddress() + " : " + ((TCPPacket)ipPacket).getSourcePort());
+		else if(ipPacket.getProtocol() == 17)
+		{	//a udp packet
+			System.out.println("UDP packet from " + ipPacket.getSourceAddress() + " : " + ((UDPPacket)ipPacket).getSourcePort());		
+		}
+		else System.out.println("Packet of unknown protocol (" +ipPacket.getProtocol() + ") from " + ipPacket.getSourceAddress());
+
+
+		System.out.println("Destination of packet : " + ipPacket.getDestinationAddress());	
+
+		byte[] data = ipPacket.getData();
+		String dataString = new String(data,"UTF-8");
+
+		System.out.println("Packet data as string : " + dataString);
+
 	}
 	
 }
+
 public class pcapreader{
 
 	public static void readFile(String filename) throws CapturePacketException {
