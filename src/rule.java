364038f0f1b4ce24587c74f15abcd5d1c9fdbc25
amanet2 <tmanetti1992@gmail.com>
2014-12-03 16:11:54 -0600

    changed a little

--- src/rule.java
+++ src/rule.java
@@ -3,8 +3,8 @@ public class rule
 	String name;
 	String type;
 	String proto;
-	String local_port; //on MY computer
-	String remote_port;	//on the sernder's computer
+	int local_port; //on MY computer
+	int remote_port;	//on the sernder's computer
 	String ip;	//the ip that the packet came from
 
 	String send;	//is this a send or recv message?	
@@ -16,8 +16,8 @@ public class rule
 		name = "blank rule";
 		type = "";	//protocol | stream
 		proto = "";	//tcp | udp
-		local_port = "";
-		remote_port = "";
+		local_port = 0;
+		remote_port = 0;
 		ip = "";
 		send = "";
 		recv = "";
